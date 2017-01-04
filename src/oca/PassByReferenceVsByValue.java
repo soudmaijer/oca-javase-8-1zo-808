e8290331f7679a0f145c4467231d7411d9b5b4a9
Stephan Oudmaijer <soudmaijer@bol.com>
2017-01-04 15:34:51 +0100

    Add sample exam questions

---
+++ src/oca/PassByReferenceVsByValue.java
@@ -0,0 +1,6 @@
+package oca;
+
+public class PassByReferenceVsByValue {
+
+    /** TRICK QUESTION! Java ONLY KNOWS PASS BY VALUE! Either a primitive value or copy if the reference (pointer value) **/
+}
